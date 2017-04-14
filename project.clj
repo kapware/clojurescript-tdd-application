525f6e5fc976895af9fe949cc3f2d8a4c19a272f
Pawel Kapala <git@kapware.com>
2017-04-14 18:51:05 +0200

    Stage one: figwheel

---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject clojurescript-tdd-application "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.227"]]
+  :plugins [[lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]
+            [lein-figwheel "0.5.6"]]
+  :clean-targets ^{:protect false} [:target-path "out" "resources/public/cljs"]
+  :cljsbuild {
+              :builds [{:id "dev"             ; development configuration
+                        :source-paths ["src"] ; Paths to monitor for build
+                        :figwheel true        ; Enable Figwheel
+                        :compiler {:main clojurescript_tdd_application.core     ; your main namespace
+                                   :asset-path "cljs/out"                       ; Where load-dependent files will go, mind you this one is relative
+                                   :output-to "resources/public/cljs/main.js"   ; Where the main file will be built
+                                   :output-dir "resources/public/cljs/out"      ; Directory for temporary files
+                                   :source-map-timestamp true}                  ; Sourcemaps hurray!
+                        }]}
+  :main ^:skip-aot clojurescript-tdd-application.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/clojurescript_tdd_application/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojurescript-tdd-application.core-test
+  (:require [clojure.test :refer :all]
+            [clojurescript-tdd-application.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
